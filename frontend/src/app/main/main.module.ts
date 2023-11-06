ceafaca52964d69dadaca06b88cfbb31e2244644
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-11-06 15:00:01 +0000

    style(layout): create basic default layout

--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PromoBannerComponent } from './components/promo-banner/promo-banner.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    DefaultLayoutComponent
+    DefaultLayoutComponent,
+    NavbarComponent,
+    PromoBannerComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
