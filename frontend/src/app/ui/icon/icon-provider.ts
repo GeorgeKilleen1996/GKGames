8f7db5d5b4a9f8557ad7b3d4b1d22623ce0e36d6
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-11-07 13:34:33 +0000

    style(logo-search-basket): add user & basket icons

--- frontend/src/app/ui/icon/icon-provider.ts
+++ frontend/src/app/ui/icon/icon-provider.ts
@@ -1,8 +1,12 @@
 import { provideIcons } from '@ng-icons/core'
 import {
   ionMenu,
+  ionPerson,
+  ionBag,
 } from '@ng-icons/ionicons'
 
 export const iconProvider = provideIcons({
   ionMenu,
+  ionPerson,
+  ionBag,
 })
